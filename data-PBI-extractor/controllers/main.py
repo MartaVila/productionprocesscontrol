5fe872b054532d3031033c4a6baf2f995f30972d
Héctor <cetohector@gmail.com>
2020-09-02 16:55:51 +0200

    Update main.py

--- data-PBI-extractor/controllers/main.py
+++ data-PBI-extractor/controllers/main.py
@@ -7,4 +7,4 @@ from odoo.http import request
 class PBIExport(CustomerPortal):
     @http.route(['/PBI/analytic/tickets'], type='http', auth="public", website=True)
     def get_tickets_analytic(self, access_token=None, report_type=None, download=False, **kw):
-        return "SI"
+        return "SIeee"
