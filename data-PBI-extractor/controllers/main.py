1c5ab7cd7bd65fa719adc3b40b0e5ffc68eabd3a
Héctor <cetohector@gmail.com>
2020-09-02 17:31:03 +0200

    Update main.py
    print

--- data-PBI-extractor/controllers/main.py
+++ data-PBI-extractor/controllers/main.py
@@ -10,43 +10,15 @@ import requests
 class PBIExport(CustomerPortal):
     @http.route(['/PBI/analytic/tickets'], type='http', auth="public", website=True)
     def get_tickets_analytic(self, access_token=None, report_type=None, download=False, **kw):
-        url = 'https://processcontrol.odoo.com'
-        db = 'marcosescano-productionprocesscontrol-master-977617'
-        username = 'hector.cerezo@processcontrol.es'
-        password = '2332AJahn2332$'
-
-        json_endpoint = "%s/jsonrpc" % url
-        headers = {"Content-Type": "application/json"}
-
-
-        def get_json_payload(service, method, *args):
-            return json.dumps({
-                "jsonrpc": "2.0",
-                "method": 'call',
-                "params": {
-                    "service": service,
-                    "method": method,
-                    "args": args
-                },
-                "id": random.randint(0, 100000000),
-            })
-
-
-        payload = get_json_payload("common", "login", db, username, password)
-        response = requests.post(json_endpoint, data=payload, headers=headers)
-        user_id = response.json()['result']
-
-        return user_id
-
-        if user_id:
-            search_domain = [['name', 'ilike', 'hector']]
-            payload2 = get_json_payload("object", "execute_kw", db, user_id, password, 'res.partner', 'search', [search_domain], {'limit': 5})
-            res = requests.post(json_endpoint, data=payload2, headers=headers).json()
-
-            payload3 = get_json_payload("object", "execute_kw", db, user_id, password, 'res.partner', 'read', [res['result']])
-            resultado = requests.post(json_endpoint, data=payload3, headers=headers).json()
-
-            return resultado
-
-        else:
-            return "Failed credentials"
+        print 'CONNECTION SUCCESSFUL ***'
+        print 'args *** ' , args
+        
+        name = args.get('name', False)
+        email = args.get('email', False)
+        
+        if not name:
+            Response.status = '400 Bad Request'
+        #return '{"response": "OK"}'
+        return request.make_response(simplejson.dumps({"response": "OK"}),
+                [('Content-Type', 'application/json'),
+                 ])
